0c2c6767b41e80aebdada607b23ccf6da12b6b33
RahmaRamadan <rahmaramadan23@gmailcom>
2022-04-13 04:50:34 +0200

    add endpoints

--- server.js
+++ server.js
@@ -4,29 +4,51 @@
 projectData = {};
 
 // Express to run server and routes
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 /* Dependencies */
-const bodyParser = require('body-parser')
+// parse url application form
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Cors for cross origin allowance, connect client and server side
-const cors = require('cors');
-const { response } = require('express');
+const cors = require("cors");
+const { response } = require("express");
 app.use(cors());
 
 /* Initializing the main project folder */
-app.use(express.static('website'));
+app.use(express.static("website"));
 
-// lisener function
-function listening(){
-    console.log("server running"); 
+// lisener function to check server
+function listening() {
+  console.log("server running");
 }
+//-------------------------------------------------------------------------
 
-// server listen "backend part"
-const server = app.listen(3000, listening)
+// GET Route
+app.get("/all", getAllProjects);
+// Callback function to complete GET '/all'
+const getAllProjects = (req, res) => {
+  return res.status(200).send(projectData);
+};
+
+//-------------------------------------------------------------------------
+
+// GET Route
+app.post("/add", postData);
+// Callback function to complete POST '/add'
+const postData = (req, res) => {
+  projectData = req.body;
+  console.log(projectData);
+  res.status(200).send(projectData);
+};
+
+//-------------------------------------------------------------------------
+
+
+// spin up the server
+app.listen(3000, listening);
